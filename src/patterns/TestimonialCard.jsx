c3eb4015be06ce390b7111e90f9e68fa964b6b78
mangel27 <angel@tpix.in>
2023-04-17 17:27:24 +0530

    avatar image size added

--- src/patterns/TestimonialCard.jsx
+++ src/patterns/TestimonialCard.jsx
@@ -77,7 +77,7 @@ const TestimonialText = ({ children }) => {
 const TestimonialAvatar = ({ icon, name, title }) => {
   return (
     <Flex align={"center"} mt={8} direction={"column"}>
-      <Avatar src={icon} mb={2} />
+      <Avatar size={"md"} src={icon} mb={2} />
       <Stack spacing={-1} align={"center"}>
         <Text fontWeight={600}>{name}</Text>
         <Text fontSize={"sm"} color={useColorModeValue("gray.600", "gray.400")}>
