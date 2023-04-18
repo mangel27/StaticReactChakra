b53cc6d9361dc79c79cd50af6abcd4a646b48cec
mangel27 <angel@tpix.in>
2023-04-18 13:55:04 +0530

    keywords added

--- src/constants/seoData.js
+++ src/constants/seoData.js
@@ -4,8 +4,8 @@ export default {
     description:
         "Halaplay is India's leading fantasy sports platform that lets you play a variety of fantasy games and win big. With over thousands of daily contests, our platform offers a range of fantasy sports including cricket, football and kabaddi. Create your own fantasy team, join leagues and compete with other players to climb up the leaderboard and win cash prizes. Our platform is safe, secure, and user-friendly, making it easy for anyone to get started. Join Halaplay today and experience the thrill of fantasy sports!",
     image: "/assets/seo_logo.png",
-    keywords:"Halaplay, fantasy sports, cricket, football, basketball, kabaddi, win big, cash prizes, India's leading, daily contests, safe, secure, user-friendly, real moneys",
     url: "https://tiny-tulumba-6cc1cf.netlify.app",
+    canonical: "https://tiny-tulumba-6cc1cf.netlify.app",
     openGraph: {
         type: "website",
         locale: "en_IE",
@@ -23,7 +23,14 @@ export default {
         handle: "@handle",
         site: "Halaplay - India's Leading Fantasy Sports Platform | Play Big and Win Big upto 10 Lakh",
         cardType: "summary_large_image"
-    }
+    },
+    additionalMetaTags: [
+
+
+       { name: 'keywords',
+        content: "Halaplay, fantasy sports, cricket, football, basketball, kabaddi, win big, cash prizes, India's leading, daily contests, safe, secure, user-friendly,real moneys"
+
+}]
 };
 export const aboutus_SEO = {
     title: "About HalaPlay – Your Daily Fantasy Sports Destination",
@@ -32,6 +39,14 @@ export const aboutus_SEO = {
     image: "/assets/seo_logo.png",
     keywords:"Halaplay, fantasy sports, cricket, football, basketball, kabaddi, win big, cash prizes, India's leading, daily contests, safe, secure, user-friendly, real moneys",
     url: "https://tiny-tulumba-6cc1cf.netlify.app/about-us",
+    canonical: "https://tiny-tulumba-6cc1cf.netlify.app/about-us",
+       additionalMetaTags: [
+
+
+       { name: 'keywords',
+        content: "Halaplay, fantasy sports, cricket, football, basketball, kabaddi, win big, cash prizes, India's leading, daily contests, safe, secure, user-friendly,real moneys"
+
+}],
     openGraph: {
         type: "website",
         locale: "en_IE",
